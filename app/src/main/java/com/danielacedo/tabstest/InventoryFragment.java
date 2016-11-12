60ae644dc93d9fe7f6814acefb8a7d2eebe7d8b6
Daniel Acedo Calderón <danielexmachina0451@gmail.com>
2016-11-13 00:18:38 +0100

    Inventory objects now change background color when selected and display its descriptions

--- app/src/main/java/com/danielacedo/tabstest/InventoryFragment.java
+++ app/src/main/java/com/danielacedo/tabstest/InventoryFragment.java
@@ -7,8 +7,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.danielacedo.tabstest.adapter.InventoryObjectRecyclerAdapter;
+import com.danielacedo.tabstest.model.InventoryObject;
 
 /**
  * Created by Daniel on 3/11/16.
@@ -22,18 +25,47 @@ public class InventoryFragment extends Fragment {
 
     RecyclerView rcv_inventoryList;
     InventoryObjectRecyclerAdapter adapter;
+    TextView txv_InventoryDescription;
     private final int SPAN_NUMBER = 3; //Number of elements per row
+
+    //RecyclerView selected element
+    InventoryObjectRecyclerAdapter.InventoryObjectHolder selectecInventoryObject;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //Inflate from layout
         View v = inflater.inflate(R.layout.inventory_fragment_layout, container, false);
 
+        txv_InventoryDescription = (TextView) v.findViewById(R.id.txv_inventoryDescription);
+
         //RecyclerView
         rcv_inventoryList = (RecyclerView)v.findViewById(R.id.rcv_inventoryList);
         rcv_inventoryList.setLayoutManager(new GridLayoutManager(container.getContext(), SPAN_NUMBER));
+
         //Set Adapter
-        adapter = new InventoryObjectRecyclerAdapter(container.getContext());
+        adapter = new InventoryObjectRecyclerAdapter(container.getContext(), new InventoryObjectRecyclerAdapter.InventoryObjectOnClick() {
+            @Override
+            public void onClick(InventoryObject inventoryObject, InventoryObjectRecyclerAdapter.InventoryObjectHolder holder) { //OnClick method to be executed for each recyclerview item
+                txv_InventoryDescription.setText(inventoryObject.getDescription()); //Display the object's description in its TextView
+
+                if(!holder.getSelected()){
+
+                    //If there was a selected item before we deselect it and change its background color back to normal
+                    if (selectecInventoryObject !=  null){
+                        selectecInventoryObject.setSelected(false);
+                        selectecInventoryObject.itemView.setBackgroundColor(getResources().getColor(R.color.tab_content_background_dark));
+                    }
+
+                    //Flag the item as selected and change its background color
+                    selectecInventoryObject = holder;
+                    holder.setSelected(true);
+                    holder.itemView.setBackgroundColor(getResources().getColor(R.color.inventoryObjectBackground_selected));
+                    holder.itemView.setBackgroundColor(getResources().getColor(R.color.inventoryObjectBackground_selected));
+                }
+
+
+            }
+        });
         rcv_inventoryList.setAdapter(adapter);
 
         return v;
--- app/src/main/java/com/danielacedo/tabstest/adapter/InventoryObjectRecyclerAdapter.java
+++ app/src/main/java/com/danielacedo/tabstest/adapter/InventoryObjectRecyclerAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.danielacedo.tabstest.InventoryTabsApplication;
 import com.danielacedo.tabstest.R;
@@ -24,17 +25,31 @@ import java.util.List;
 public class InventoryObjectRecyclerAdapter extends RecyclerView.Adapter<InventoryObjectRecyclerAdapter.InventoryObjectHolder> {
 
     private List<InventoryObject> inventoryObjectList;
+    private InventoryObjectOnClick onClickListener;
     private Context context;
 
-    public InventoryObjectRecyclerAdapter(Context context){
+    //Interface for callback OnClick method to be implemented in the view
+    public interface InventoryObjectOnClick{
+        void onClick(InventoryObject inventoryObject, InventoryObjectHolder holder);
+    }
+
+    public InventoryObjectRecyclerAdapter(Context context, InventoryObjectOnClick listener){
         this.context = context;
         this.inventoryObjectList = new ArrayList<InventoryObject>();
         this.inventoryObjectList.addAll(InventoryTabsApplication.getInventoryObjectList());
+        this.onClickListener = listener;
     }
 
     @Override
-    public void onBindViewHolder(InventoryObjectHolder holder, int position) {
+    public void onBindViewHolder(final InventoryObjectHolder holder, final int position) {
         holder.txv_inventoryObjectName.setText(inventoryObjectList.get(position).getName());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickListener.onClick(inventoryObjectList.get(position), holder);
+            }
+        });
 
         if(inventoryObjectList.get(position).isStackable()){
             holder.txv_inventoryObjectQuantity.setText("x"+String.valueOf(inventoryObjectList.get(position).getQuantity()));
@@ -48,6 +63,7 @@ public class InventoryObjectRecyclerAdapter extends RecyclerView.Adapter<Invento
     public InventoryObjectHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.inventoryobject_layout, parent, false);
 
+
         return new InventoryObjectHolder(item);
     }
 
@@ -56,18 +72,33 @@ public class InventoryObjectRecyclerAdapter extends RecyclerView.Adapter<Invento
         return inventoryObjectList.size();
     }
 
+    public List<InventoryObject> getAll(){
+        return inventoryObjectList;
+    }
+
     public static class InventoryObjectHolder extends RecyclerView.ViewHolder{
         ImageView imv_inventoryObjectImage;
         TextView txv_inventoryObjectName;
         TextView txv_inventoryObjectQuantity;
 
+        boolean isSelected;
 
         public InventoryObjectHolder(View v) {
             super(v);
             imv_inventoryObjectImage = (ImageView)v.findViewById(R.id.imv_inventoryObjectImage);
             txv_inventoryObjectName = (TextView)v.findViewById(R.id.txv_inventoryObjectName);
             txv_inventoryObjectQuantity = (TextView)v.findViewById(R.id.txv_inventoryObjectQuantity);
+            isSelected = false;
         }
+
+        public boolean getSelected(){
+            return isSelected;
+        }
+
+        public void setSelected(boolean value){
+            isSelected = value;
+        }
+
     }
 
 
